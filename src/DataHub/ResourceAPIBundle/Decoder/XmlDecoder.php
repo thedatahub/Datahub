3e3ba0d0834c52216618762e9269a9db50648ba0
Matthias Vandermaesen <matthias@colada.be>
2017-06-22 10:33:19 +0200

    removed: yanked out non-functional conversion from Clark to Catmandu.

--- src/DataHub/ResourceAPIBundle/Decoder/XmlDecoder.php
+++ src/DataHub/ResourceAPIBundle/Decoder/XmlDecoder.php
@@ -48,63 +48,9 @@ class XmlDecoder implements DecoderInterface
     {
         try {
             $result = $this->converter->read($data);
-
-            $build = [];
-            $this->convertClarkToCatmandu($result, $build);
-
-            return array($build);
+            return $result;
         } catch (\Exception $e) {
             throw new BadRequestHttpException('Invalid XML: ' . $e->getMessage());
-        }
-    }
-
-    /**
-     * Convert Clark Notation to Catmandu Notation.
-     *
-     * This function will recursively traverse through a $result array which is
-     * structured in Clark Notation and convert it to 'Catmandu' notation.
-     *
-     * @todo
-     *   This is legacy code. Needs to be moved to a proper class and injected
-     *   via the container based on an configuration option. Or we could use an
-     *   event listener.
-     *
-     * @param $raw array The raw input array in Clark Notation.
-     * @param $build array An empty build array which will be constructed as the
-     *     function traverses through the $raw array.
-     *
-     * @return void
-     */
-    public function convertClarkToCatmandu(&$raw, &$build) {
-        foreach ($raw as $key => &$item) {
-            if (isset($item['name'])) {
-                // Remove namespace from the element name
-                $item['name'] = preg_replace('/{.*}(.*)/', '$1', $item['name']);
-
-                $build[$item['name']] = [];
-
-                // Re-arrange the 'value' key/pair of the current node
-                if (is_array($item['value']) && !is_null($item['value'])) {
-                    $this->convertClarkToCatmandu($item['value'], $build[$item['name']]);
-                    $contents = $item['value'];
-                } else {
-                    $contents = ['_' => $item['value'] ];
-                }
-
-                // Remove namespace from attributes names
-                $item['attributes'] = array_flip(
-                    array_map(function($value) {
-                        return preg_replace('/{.*}(.*)/', '$1', $value);
-                    }, array_flip($item['attributes']))
-                );
-                $contents = array_merge($contents, $item['attributes']);
-
-                // Put it all back together, use the element name as the key for
-                // the current node
-
-                $build[$item['name']] = $contents;
-                $item = [ $item['name'] => $contents ];
-            }
         }
     }
 }
