37afe74293c77382059a95687484cc012744684e
Matthias Vandermaesen <matthias@colada.be>
2018-07-20 22:23:28 +0200

    Changed: AnnotationDriver::registerAnnotationClasses was deprecated in doctrine/mongodb-odm 1.2.1.

--- app/autoload.php
+++ app/autoload.php
@@ -8,6 +8,5 @@ use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-AnnotationDriver::registerAnnotationClasses();
 
 return $loader;
